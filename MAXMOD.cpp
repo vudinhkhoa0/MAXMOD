a6f287b1726f0b15085c3ab6da92d7f403ce58e1
vudinhkhoa0 <126996254+vudinhkhoa0@users.noreply.github.com>
2023-07-23 00:08:42 +0700

    Add files via upload

---
+++ MAXMOD.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    for (int k = 1; k <= t; k++)
+    {
+        long long x;
+        cin >> x;
+        cout << (x / 2) + 1;
+        cout << endl;
+    }
+    return 0;
+}
+
